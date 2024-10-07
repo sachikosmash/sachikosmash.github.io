c0a466b112fd287ccf3148a95e899eed5db94bb2
vubblechi <sachi.ruaya@smash.org.au>
2024-10-07 23:01:53 +1100

    update 3

--- script.js
+++ script.js
@@ -8,11 +8,85 @@ menuToggle.classList.toggle('active');
 showcase.classList.toggle('active');
 })
 
+const videoList = [
+    { name: "Modal", link: "modal.mp4" },
+];
+
+{
+    myVideo.pause(); // Pause the video before changing source
+    console.log(videoList[0].link);
+    myVideo.src = videoList[0].link;
+    myVideo.load(); // Load the new source
+    myVideo.play(); // Play the video
+}
+
+let loop = true;
+
 //media player interactives
-const step1Btn = document.querySelector("#step-1-btn");
-console.log(step1Btn);
-step1Btn.addEventListener("click", gotoStep1);
+const myVideo = document.querySelector("#my-video");
+const videoName = document.querySelector("#video-name");
+const videoTime = document.querySelector("#video-time");
+const progressBar = document.querySelector("#progress-bar-fill");
+
+const step1Button = document.querySelector("#Incubation");
+console.log(step1Button)
+step1Button.addEventListener("click", gotoStep1); 
 
 function gotoStep1() {
-mediaPlayer.currentTime = 0.56;
-} 
+    myVideo.currentTime = 0.01;
+    if (myVideo.paused) {
+        myVideo.play();
+    }
+}
+
+const step2Button = document.querySelector("#Static");
+console.log(step2Button)
+step1Button.addEventListener("click", gotoStep2); 
+
+function gotoStep2() {
+    myVideo.currentTime = 0.56;
+    if (myVideo.paused) {
+        myVideo.play();
+    }
+}
+
+const step3Button = document.querySelector("#Ripple");
+console.log(step3Button)
+step1Button.addEventListener("click", gotoStep3); 
+
+function gotoStep3() {
+    myVideo.currentTime = 1.31;
+    if (myVideo.paused) {
+        myVideo.play();
+    }
+}
+
+const step4Button = document.querySelector("#Aurora");
+console.log(step4Button)
+step1Button.addEventListener("click", gotoStep4); 
+
+function gotoStep4() {
+    myVideo.currentTime = 2.30;
+    if (myVideo.paused) {
+        myVideo.play();
+    }
+}
+
+const step5Button = document.querySelector("#Fireworks");
+console.log(step5Button)
+step1Button.addEventListener("click", gotoStep5); 
+
+function gotoStep5() {
+    myVideo.currentTime = 2.30;
+    if (myVideo.paused) {
+        myVideo.play();
+    }
+}
+
+const step6Button = document.querySelector("#Swamp");
+console.log(step6Button)
+step1Button.addEventListener("click", gotoStep6); 
+
+const step7Button = document.querySelector("#Particle");
+console.log(step7Button)
+step1Button.addEventListener("click", gotoStep7); 
